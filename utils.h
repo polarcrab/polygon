86c8683d086d7c2b0a7f8182c8930d7e9dcc83a7
Harmandeep Singh <harmandeep.singh1@delhivery.com>
2017-07-04 13:42:54 +0530

    COde level documentation added

--- utils.h
+++ utils.h
@@ -1,3 +1,11 @@
+/**
+    Header file from utils.cpp
+
+    @author Harmandeep Singh
+    @email harmandeep DOT singh1 AT delhivery DOT com
+    @date 3 July 2017
+*/
+
 #ifndef UTILS_H
 #define UTILS_H
 #include <map>
@@ -11,13 +19,12 @@ bool is_close(double val1, double val2);
 double cal_length(Edge edge);
 double cal_decision_length(Point &p, Edge &e);
 Point closest_point(Edge &e, PointVector& ip,
-                   std::map<Point,
-                   std::pair<Point, Point>> &neighbours);
+                    std::map<Point,
+                    std::pair<Point, Point>> &neighbours);
 double cal_angle(const Point& p1, const Point& p2, const Point& p3);
 double cal_angle_with_X_axis(const Point &a, const Point &b);
 double cal_dis(const Point& p1, const Point& p2);
-bool in_range(double x1, double y1, double x2, double y2,
-             double x0, double y0);
+bool in_range(Point &p1, Point &p2, Point &p0);
 bool check_intersections(std::list<Edge> edges, Edge e1);
 class AngleSort
 {
